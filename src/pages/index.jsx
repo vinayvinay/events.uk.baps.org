966746b767fab0443198d9939d8e895e67c84033
Vinay P <vinay@gocardless.com>
2020-03-04 22:46:48 +0000

    Mobile version of landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,7 +15,21 @@ const IndexPage = () => {
           }
         }
       }
+      msm2020LogoDark: file(relativePath: { eq: "msm2020-logo-dark.png" }) {
+        childImageSharp {
+          fluid(quality: 80) {
+            ...GatsbyImageSharpFluid_noBase64
+          }
+        }
+      }
       lm25Logo: file(relativePath: { eq: "lm25-logo-monochrome.png" }) {
+        childImageSharp {
+          fluid(quality: 80) {
+            ...GatsbyImageSharpFluid_noBase64
+          }
+        }
+      }
+      lm25LogoColour: file(relativePath: { eq: "lm25-logo.png" }) {
         childImageSharp {
           fluid(quality: 80) {
             ...GatsbyImageSharpFluid_noBase64
@@ -75,108 +89,188 @@ const IndexPage = () => {
   `)
 
   return (
-    <div class="flex mx-auto fix-aspect">
-      <div class="w-1/3 group invisible animated fadeInDownBig delay-1s faster">
-        <div class="group-hover:hidden relative">
-          <Img
-            fluid={images.msm2020Backdrop.childImageSharp.fluid}
-            alt="MSM2020 Backdrop"
-            loading="eager"
-          />
-          <div class="w-3/4 mx-auto pb-10 absolute bottom-0 inset-x-0 animated fadeIn fast">
+    <div>
+      <div class="hidden md:flex mx-auto fix-aspect">
+        <div class="w-1/3 group invisible animated fadeIn delay-1s">
+          <div class="group-hover:hidden relative">
             <Img
-              fluid={images.msm2020Logo.childImageSharp.fluid}
-              alt="MSM2020"
+              fluid={images.msm2020Backdrop.childImageSharp.fluid}
+              alt="MSM2020 Backdrop"
               loading="eager"
             />
-          </div>
-        </div>
-        <div class="hidden group-hover:block overflow-hidden relative">
-          <div class="zoom-on-hover">
-            <a href="/msm2020">
+            <div class="w-3/4 mx-auto pb-10 absolute bottom-0 inset-x-0 animated fadeIn fast">
               <Img
-                fluid={images.msm2020BackdropRaw.childImageSharp.fluid}
-                alt="MSM2020 Backdrop Raw"
+                fluid={images.msm2020Logo.childImageSharp.fluid}
+                alt="MSM2020"
                 loading="eager"
               />
-              <div class="w-full mx-auto pb-12 text-center absolute bottom-0 inset-x-0">
-                <span class="title-shadow text-xl lg:text-2xl font-light text-gray-100 animated fadeIn slow">
-                  His Holiness Mahant Swami Maharaj in &nbsp;
-                  <br class="hidden lg:block" />
-                  UK &amp; Europe 2020
-                </span>
-              </div>
-            </a>
+            </div>
+          </div>
+          <div class="hidden group-hover:block overflow-hidden relative">
+            <div class="zoom-on-hover">
+              <a href="/msm2020">
+                <Img
+                  fluid={images.msm2020BackdropRaw.childImageSharp.fluid}
+                  alt="MSM2020 Backdrop Raw"
+                  loading="eager"
+                />
+                <div class="w-full mx-auto pb-12 text-center absolute bottom-0 inset-x-0">
+                  <span class="title-shadow text-xl lg:text-2xl font-light text-gray-100 animated fadeIn slow">
+                    His Holiness Mahant Swami Maharaj in &nbsp;
+                    <br class="hidden lg:block" />
+                    UK &amp; Europe 2020
+                  </span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="w-1/3 group invisible animated fadeIn delay-2s">
+          <div class="group-hover:hidden relative">
+            <Img
+              fluid={images.lm25Backdrop.childImageSharp.fluid}
+              alt="LM25 Backdrop"
+              loading="eager"
+            />
+            <div class="w-1/2 mx-auto pb-8 absolute bottom-0 inset-x-0 animated fadeIn fast">
+              <Img
+                fluid={images.lm25Logo.childImageSharp.fluid}
+                alt="LM25"
+                loading="eager"
+              />
+            </div>
+          </div>
+          <div class="hidden group-hover:block overflow-hidden relative">
+            <div class="zoom-on-hover">
+              <a href="/lm25">
+                <Img
+                  fluid={images.lm25BackdropRaw.childImageSharp.fluid}
+                  alt="LM25 Backdrop Raw"
+                  loading="eager"
+                />
+                <div class="w-full mx-auto pb-12 text-center absolute bottom-0 inset-x-0">
+                  <span class="title-shadow text-xl lg:text-2xl font-light text-gray-100 animated fadeIn slow">
+                    25th Anniversary of &nbsp;
+                    <br class="hidden lg:block" />
+                    BAPS Shri Swaminarayan Mandir, London
+                  </span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="w-1/3 group invisible animated fadeIn delay-3s">
+          <div class="group-hover:hidden relative">
+            <Img
+              fluid={images.psm100Backdrop.childImageSharp.fluid}
+              alt="PSM100 Backdrop"
+              loading="eager"
+            />
+            <div class="w-1/2 mx-auto pb-8 absolute bottom-0 inset-x-0 animated fadeIn fast">
+              <Img
+                fluid={images.psm100Logo.childImageSharp.fluid}
+                alt="PSM100"
+                loading="eager"
+              />
+            </div>
+          </div>
+          <div class="hidden group-hover:block overflow-hidden relative">
+            <div class="zoom-on-hover">
+              <a href="/psm100">
+                <Img
+                  fluid={images.psm100BackdropRaw.childImageSharp.fluid}
+                  alt="PSM100 Backdrop Raw"
+                  loading="eager"
+                />
+                <div class="w-full mx-auto pb-12 text-center absolute bottom-0 inset-x-0">
+                  <span class="title-shadow text-xl lg:text-2xl font-light text-gray-100 animated fadeIn slow">
+                    Centenary Birth Anniversary of &nbsp;
+                    <br class="hidden lg:block" />
+                    His Holiness Pramukh Swami Maharaj
+                  </span>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      <div class="w-1/3 group invisible animated fadeInDownBig delay-2s faster">
-        <div class="group-hover:hidden relative">
-          <Img
-            fluid={images.lm25Backdrop.childImageSharp.fluid}
-            alt="LM25 Backdrop"
-            loading="eager"
-          />
-          <div class="w-1/2 mx-auto pb-8 absolute bottom-0 inset-x-0 animated fadeIn fast">
-            <Img
-              fluid={images.lm25Logo.childImageSharp.fluid}
-              alt="LM25"
-              loading="eager"
-            />
-          </div>
-        </div>
-        <div class="hidden group-hover:block overflow-hidden relative">
-          <div class="zoom-on-hover">
-            <a href="/lm25">
+
+      <div class="mobile md:hidden text-center">
+        <a href="/lm25">
+          <div class="flex w-11/12 h-48 mx-auto mt-2 mb-6 overflow-hidden rounded-lg bg-white animated fadeIn slow delay-1s">
+            <div class="w-2/5">
               <Img
                 fluid={images.lm25BackdropRaw.childImageSharp.fluid}
                 alt="LM25 Backdrop Raw"
                 loading="eager"
               />
-              <div class="w-full mx-auto pb-12 text-center absolute bottom-0 inset-x-0">
-                <span class="title-shadow text-xl lg:text-2xl font-light text-gray-100 animated fadeIn slow">
-                  25th Anniversary of &nbsp;
-                  <br class="hidden lg:block" />
-                  BAPS Shri Swaminarayan Mandir, London
-                </span>
+            </div>
+            <div class="w-3/5 my-auto">
+              <div class="px-10">
+                <Img
+                  fluid={images.lm25LogoColour.childImageSharp.fluid}
+                  alt="LM25"
+                  loading="eager"
+                />
               </div>
-            </a>
+              <div class="p-3 text-xs font-light text-yellow-700">
+                25th Anniversary of
+                <br />
+                BAPS Shri Swaminarayan Mandir, London
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="w-1/3 group invisible animated fadeInDownBig delay-3s faster">
-        <div class="group-hover:hidden relative">
-          <Img
-            fluid={images.psm100Backdrop.childImageSharp.fluid}
-            alt="PSM100 Backdrop"
-            loading="eager"
-          />
-          <div class="w-1/2 mx-auto pb-8 absolute bottom-0 inset-x-0 animated fadeIn fast">
-            <Img
-              fluid={images.psm100Logo.childImageSharp.fluid}
-              alt="PSM100"
-              loading="eager"
-            />
+        </a>
+        <a href="/msm2020">
+          <div class="flex w-11/12 h-48 mx-auto mt-2 mb-6 overflow-hidden rounded-lg bg-white animated fadeIn slow delay-2s">
+            <div class="w-2/5">
+              <Img
+                fluid={images.msm2020BackdropRaw.childImageSharp.fluid}
+                alt="MSM2020 Backdrop Raw"
+                loading="eager"
+              />
+            </div>
+            <div class="w-3/5 my-auto">
+              <div class="px-5">
+                <Img
+                  fluid={images.msm2020LogoDark.childImageSharp.fluid}
+                  alt="MSM2020"
+                  loading="eager"
+                />
+              </div>
+              <div class="pt-5 text-xs font-light text-yellow-700">
+                HH Mahant Swami Maharaj in
+                <br />
+                UK &amp; Europe 2020
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="hidden group-hover:block overflow-hidden relative">
-          <div class="zoom-on-hover">
-            <a href="/psm100">
+        </a>
+        <a href="/psm100">
+          <div class="flex w-11/12 h-48 mx-auto mt-2 mb-6 overflow-hidden rounded-lg bg-white animated fadeIn slow delay-3s">
+            <div class="w-2/5">
               <Img
                 fluid={images.psm100BackdropRaw.childImageSharp.fluid}
                 alt="PSM100 Backdrop Raw"
                 loading="eager"
               />
-              <div class="w-full mx-auto pb-12 text-center absolute bottom-0 inset-x-0">
-                <span class="title-shadow text-xl lg:text-2xl font-light text-gray-100 animated fadeIn slow">
-                  Centenary Birth Anniversary of &nbsp;
-                  <br class="hidden lg:block" />
-                  His Holiness Pramukh Swami Maharaj
-                </span>
+            </div>
+            <div class="w-3/5 my-auto">
+              <div class="px-5">
+                <Img
+                  fluid={images.psm100Logo.childImageSharp.fluid}
+                  alt="PSM100"
+                  loading="eager"
+                />
               </div>
-            </a>
+              <div class="text-xs font-light text-yellow-700">
+                Centenary Birth Anniversary of
+                <br />
+                HH Pramukh Swami Maharaj
+              </div>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   )
